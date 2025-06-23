4d8e4f0964716fab7ba4b920f48d56c98ba79bfb
Aldian0 <alsanahh409@gmail.com>
2025-06-23 12:07:53 +0700

    change

--- addons/ds_base_hospital/models/pasien.py
+++ addons/ds_base_hospital/models/pasien.py
@@ -1,20 +1,16 @@
 from odoo import models, fields, api
-
-
-# Data pasien tambahan, extend res.partner (data pasien (NIK, nama, tanggal lahir, no. RM, dll.)
-
+# from odoo.exceptions import ValidationError
 
 class DsPAsien(models.Model):
     _name = 'ds.pasien'
     _description = 'Ds Pasien'
 
-# class DsPasien(models.Model):
-#     _name = 'ds.Pasien'
-#     _description = 'Ds Pasien'
+
 
     name = fields.Char(string='Nama Lengkap', required=True)
+    age = fields.Integer(string='Umur')
     nik = fields.Char(string='NIK', size=16)
     gender = fields.Selection([('male','Male'),('female','Female')], string='Jenis Kelamin')
-    birth_date = fields.Date(string='Birth Date')
+    birth_date = fields.Date(string='Tanggal Lahir')
     phone = fields.Char(string='Nomor Telepon')
     address = fields.Text(string='Alamat')
--- addons/ds_base_hospital/models/registrasi.py
+++ addons/ds_base_hospital/models/registrasi.py
@@ -1,2 +1,2 @@
-from odoo import models, fields
+
 # Data pendaftaran/antrian pasien (data pendaftaran (tanggal, poli, dokter, pasien_id)
